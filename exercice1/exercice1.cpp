b728dce788354c9cb7e20c6a16cfbaedaee937c9
Aladin-bit01 <alaaeddinaddas11@gmail.com>
2024-01-20 11:51:39 -0500

    exo1 fait (norme de codage pas pris en consideration, verification a la fin)

--- exercice1/exercice1.cpp
+++ exercice1/exercice1.cpp
@@ -3,45 +3,70 @@
 #include <vector>
 using namespace std;
 
-int main() {
-    string phrase;
+vector<string> splitSentence(string);
+string longestWord(vector<string>);
+string shortestWord(vector<string>);
+double averageLength(vector<string>);
 
+int main() {
+    string sentence; 
     cout << "Saisissez une phrase: ";
-    cin >> phrase;
-    cout << phrase;
+    getline(cin, sentence);
+    vector<string> sentenceWords=  splitSentence(sentence);  
+    string shortest = shortestWord(sentenceWords);
+    string longest = longestWord(sentenceWords);
+    double average =  averageLength(sentenceWords);
+    cout << "Le mot le plus court est : " << shortest << endl;
+    cout << "Le mot le plus long est : " << longest << endl;
+    cout << "La longueur moyenne est : " << average << " lettres";
 }
 
+vector<string> splitSentence(string phrase) {
+    string word;
+    vector <string> sentenceWords;
+    for (char letter : phrase) {
+        if (letter != ' ') {
+            word += letter;
+        }
+        else {
+            sentenceWords.push_back(word);
+            word.clear();
+        }
+    }
+    sentenceWords.push_back(word);
+    return sentenceWords;
+}
 
+string shortestWord(vector<string>listWords) {
+    int length = listWords[1].length();
+    string shortest = " ";
+    for (auto word : listWords) {
+        if (word.length() < length) {
+            length = word.length();
+            shortest = word;
+        }
+    }
+    return shortest;
+}
 
+string longestWord(vector<string> listWords) {
+    int length = 0;
+    string longest = " ";
+    for (auto word : listWords) {
+        if (word.length() > length) {
+            length = word.length();
+            longest = word;
+        }
+    }
+    return longest;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //string phrase;
-    //string word;
-    //cout << "Saisissez une phrase : ";
-    //cin >> phrase;
-    //for (char letter : phrase) {
-    //    if (letter != ' ') {
-    //        word += letter;
-
-    //    }
-    //     }
-    //cout << word;
+double averageLength(vector<string> listWords) {
+    double average = 0.0;
+    for (auto word : listWords) {
+        average += word.length();
+    }
+    average = average / size(listWords);
+    return average;
 }
 
